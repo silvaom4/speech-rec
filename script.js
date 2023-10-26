bfc14592d12206307b7f8acf7eede3ee969b1fd3
silvaom4 <silvaaom@gmail.com>
2023-10-26 15:36:18 -0400

    Added JS to speak recognition

---
+++ script.js
@@ -0,0 +1,34 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.interimResults =  true;
+recognition.lang = 'en-US';
+// recognition.lang = 'es-ES';
+
+
+const words = document.querySelector('.words');
+let p = document.createElement('p');
+words.append(p);
+
+
+recognition.addEventListener('result', function(e) {
+    // console.log(e.results)
+    const transcript = Array.from(e.results)
+    .map((result)=> result[0])
+    .map((result) => result.transcript)
+    .join('');
+
+    p.textContent = transcript;
+    if(e.results[0].isFinal) {
+        p = document.createElement('p')
+        words.appendChild(p);
+    }
+
+})
+
+recognition.addEventListener('end', recognition.start)
+
+
+
+recognition.start();
